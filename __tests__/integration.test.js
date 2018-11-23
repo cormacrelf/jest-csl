79f7393a9b5d4e9fd228609a00950ad4a4cfa654
Cormac Relf <web@cormacrelf.net>
2018-11-23 18:12:02 +1100

    add integration tests

---
+++ __tests__/integration.test.js
@@ -0,0 +1,12 @@
+// Based on https://github.com/citation-style-language/test-suite/blob/master/processor-tests/humans/integration_SimpleIbid.txt
+
+module.exports = {
+  csl: "./__tests__/integration.csl",
+  libraries: ["./__tests__/integration.json"],
+  suites: ["./__tests__/integration.yaml"],
+  output: "./results-ibid.json"
+}
+
+const { jestCSL } = require('../index');
+jestCSL(module.exports);
+
---
+++ __tests__/results.test.js
@@ -0,0 +1,21 @@
+const { cslTestResults } = require('../index');
+let config = require('./integration.test');
+
+describe("results object", () => {
+  let { engine, library, citeIds, units } = cslTestResults(config);
+  it("includes an item used in the test", () => {
+    expect(citeIds).toContain('ITEM-1');
+    expect(library).not.toBeNull();
+    expect(library['ITEM-1']).not.toBeNull();
+    expect(library['ITEM-1'].type).toBe('book');
+  });
+  it("has a passing test", () => {
+    let u = units[0]
+    expect(u && u.tests).not.toBeNull();
+    let t = u.tests[0];
+    expect(t).not.toBeNull();
+    expect(t.passed).toBe(true);
+    expect(t.result).toMatchObject(t.expect);
+  });
+});
+
